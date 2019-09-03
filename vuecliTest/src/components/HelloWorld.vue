af56287aeab657f2d5b791dbacd1fdc788b5893f
sunshineJing <sunjing9987@163.com>
2019-09-03 16:22:27 +0800

    four

--- vuecliTest/src/components/HelloWorld.vue
+++ vuecliTest/src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-   
   </div>
 </template>
 
@@ -10,7 +9,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js Appa'
+      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
